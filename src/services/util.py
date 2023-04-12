a6f6da99df681cc942633a8dcc2b2fdbdd1d1877
hector ramon leon <payorayo@hectors-MacBook-Pro.local>
2023-04-11 19:09:13 -0700

    Feat: refactor code

--- src/services/util.py
+++ src/services/util.py
@@ -1,9 +1,7 @@
-import base64
 import json
 import os
 from typing import List
 
-import jwt
 import requests
 
 from src.data import schema
@@ -24,29 +22,6 @@ def get_content_json(filename: str) -> dict:
     with open(f"{os.path.dirname(schema.__file__)}/{filename}.json") as f:
         data = json.load(f)
     return data
-
-
-def encode_token(content: dict):
-    """
-    encode token
-    :param content:
-    :return:
-    """
-    return jwt.encode(content, conf_service.SECRET_JWT, algorithm="HS256")
-
-
-def decode_token(token: str):
-    """
-    decode token
-    :param token:
-    :return:
-    """
-    return jwt.decode(token, conf_service.SECRET_JWT, algorithm="HS256")
-
-
-def encrypt_password(password: str) -> str:
-    password = password.encode('utf-8')
-    return base64.b64encode(password).decode()
 
 
 def get_random_string(length_string: str) -> requests.Response:
